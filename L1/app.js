80d1d0d7a93fdb2d419bd070c39064f1b8e7af20
Amir Sohail <amirsohail@Amirs-MacBook-Air.local>
2024-12-28 17:23:26 +0530

    Typescript file

---
+++ L1/app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = 10;
+var n2 = 20;
+var result = add(n1, n2);
+console.log(result); // 30
---
+++ L1/app.ts
@@ -0,0 +1,8 @@
+function add(n1, n2){
+    return n1 + n2;
+}
+
+const n1 = 10;
+const n2 = 20;
+const result = add(n1, n2);
+console.log(result); // 30
