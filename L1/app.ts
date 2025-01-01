7eced4f21f2b07352014b28f5372a063ee3d0648
Amir Sohail <amirsohail@Amirs-MacBook-Air.local>
2025-01-01 18:13:47 +0530

    Typescript file

--- L1/app.ts
+++ L1/app.ts
@@ -1,8 +1,16 @@
-function add(n1, n2){
-    return n1 + n2;
+function add(n1, n2) {
+  return n1 + n2;
 }
 
 const n1 = 10;
 const n2 = 510;
 const result = add(n1, n2);
 console.log(result); // 30
+
+function printName(name: string): void {
+  console.log(`Name: ${name}`);
+}
+
+// Example usage
+printName("Alice");
+printName("Bob");
