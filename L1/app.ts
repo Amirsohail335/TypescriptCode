cfc4b0957eccdd32edb8194ce73fcf214174ef89
Amirsohail335 <amirsohail335@gmail.com>
2025-01-07 23:18:39 +0530

    Typescript code

--- L1/app.ts
+++ L1/app.ts
@@ -4,12 +4,19 @@ function add(n1, n2) {
 
 const n1 = 10;
 const n2 = 10000;
-const result = add(n1, n2);
-console.log(result); // 30
+// const result = add(n1, n2);
+// console.log(result); // 30
 
 function printName(name: string): void {
   console.log(`Name: ${name}`);
 }
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+// Example usage
+const result = subtract(10, 4);
+console.log(`Result: ${result}`); // Output: Result: 6
 
 // Example usage
 printName("Alice");
