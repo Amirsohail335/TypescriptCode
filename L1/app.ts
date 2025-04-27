d62a2d92b2c94c61a89bb60eae82f940c1fb3334
Amirsohail335 <amirsohail335@gmail.com>
2025-04-27 12:56:07 +0530

    c

--- L1/app.ts
+++ L1/app.ts
@@ -28,7 +28,7 @@ console.log(`Result of multiply ${multiplyResult}`);
 // Example usage
 printName("Alicy");
 printName("hi");
-printName("Charlie deen");
+printName("Charlie dean deen");
 printName("Shiza Amreens");
 printName("Bob");
 // Output: Name: Alice, Name: Bob, Name: Charlie
