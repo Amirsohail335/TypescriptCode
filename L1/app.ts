d1eed446e17322ff3da66e8ebbc1dd68a2a08eb9
Amirsohail335 <amirsohail335@gmail.com>
2025-06-02 23:52:55 +0530

    pig game

--- L1/app.ts
+++ L1/app.ts
@@ -28,7 +28,7 @@ console.log(`Result of multiply ${multiplyResult}`);
 // Example usage
 printName("Alicy1");
 printName("hi");
-printName("Charlie dean deen");
+printName("Charlie dean deeen");
 printName("Shiza Amreens");
 printName("Bob");
 // Output: Name: Alice, Name: Bob, Name: Charlie
